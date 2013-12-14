5adb365a25a6abf706c40291056bfd78b4c4f60e
Max Corman <cormamax@umich.edu>
2013-12-14 14:27:10 -0800

    added inmovable object
    fixed inmovable object

---
+++ LudumDare28/Assets/Scripts/UnmovableObjectScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnmovableObjectScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    /*
+     * Do all physics based updates here. Currently, not movable objects shouldn't do anything
+     * but we may want to change that later.
+    */
+    void FixedUpdate()
+    {
+        
+    }
+}
